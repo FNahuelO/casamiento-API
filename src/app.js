f0e63662116b792e6aa4eacb64e70c2337b518dd
FNahuelO <fosorio@aidforaids.org>
2023-09-25 22:31:20 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express, { urlencoded, json } from "express";
 import { router } from "./routes/index.js";
 import { config } from "dotenv";
-import cors from "cors";
 
 config();
 
@@ -11,12 +10,14 @@ export const server = express();
 
 server.use(urlencoded({ extended: true, limit: "50mb" }));
 server.use(json({ limit: "50mb" }));
-server.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-  })
-);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 server.use("/", router);
